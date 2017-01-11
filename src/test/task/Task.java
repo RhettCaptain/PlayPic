412537f1d1b4ae2a7d58208d430dcf66ed79a408
rhettcaptain <425237277@qq.com>
2017-01-11 20:18:29 +0800

    add delete

--- src/test/task/Task.java
+++ src/test/task/Task.java
@@ -15,7 +15,8 @@ public class Task {
     	//获取当前分钟数
     	Calendar cal = Calendar.getInstance();
     	int curMinute = cal.get(Calendar.MINUTE);
-    	//
+    	//超过dropMinute的图片则删除
+    	int dropMinute = 5;
     	for(String path:uploadPics){
     		File f = new File(uploadPath.getPath() + "\\" + path);
     		long picTime = f.lastModified();
@@ -26,7 +27,21 @@ public class Task {
     		if(delMinute < 0){
     			delMinute += 60;	//相差时间应当为正
     		}
-    		if(delMinute > 5){
+    		if(delMinute > dropMinute){
+    			f.delete();
+    		}
+    	}
+    	for(String path:dealPics){
+    		File f = new File(uploadPath.getPath() + "\\" + path);
+    		long picTime = f.lastModified();
+    		cal.setTimeInMillis(picTime);
+    		int picMinute = cal.get(Calendar.MINUTE);
+    		//计算相差的时间
+    		int delMinute = curMinute - picMinute;
+    		if(delMinute < 0){
+    			delMinute += 60;	//相差时间应当为正
+    		}
+    		if(delMinute > dropMinute){
     			f.delete();
     		}
     	}
